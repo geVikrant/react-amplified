fa5259164ff3640184a4a3d22db906fec6e97194
502675409 <vikrant.jagtap@ge.com>
2020-11-18 17:24:27 -0600

    US23424 changes

--- src/App.js
+++ src/App.js
@@ -22,4 +22,19 @@ function App() {
   );
 }
 
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Taylor" />,
+  document.getElementById('hello-example')
+);
+
 export default App;
